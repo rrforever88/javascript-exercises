4e7974fdc3621e35f510f3b3ebcb2d3e30ec5aae
christophernelson <irish.claret@gmail.com>
2026-08-15 21:21:05 -0400

    Completed 08_removeFromArray. All test passed.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    const result = array.filter(item => !items.includes(item));
+    return result
 };
 
 // Do not edit below this line
