39752fe1b6bfe8dde1c5ef215fe2f8cde782f4ce
Chris Nelson <chris.nelson@sap.com>
2026-08-25 10:22:56 -0400

    Completed 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } 
+    
+    if (num === 0 || num === "0") {
+        return 0;
+    }
+
+    let fib = [];
+    let first = 0;
+    let last = 1;
+    let result = 0;
+
+    for(let i = 1; i <= num; ++i) {
+        // add first and last
+        result = first + last;
+        // append to fib
+        fib.push(result);
+        // update last to first
+        last = first;
+        first = result;
+    }
+
+    return fib.length > 0 ? fib.at(-1) : 1;
 
 };
 
