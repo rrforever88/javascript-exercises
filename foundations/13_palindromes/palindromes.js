46f5ff24f7a28b09eea13eec72aeede78ed0858d
Chris Nelson <chris.nelson@sap.com>
2026-08-25 09:58:10 -0400

    Completed 13_palinfromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove puncuation and spaces
+    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+    let cleanStrReverse = cleanStr.split("").reverse().join("");
+
+    return cleanStr === cleanStrReverse;
 
 };
 
