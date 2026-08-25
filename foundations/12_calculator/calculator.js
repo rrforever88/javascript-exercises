7b4e8ced3ea3c1f3d26d9c1b85ba413852aef370
Chris Nelson <chris.nelson@sap.com>
2026-08-25 09:51:02 -0400

    Completed 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+
+  for(let i = 0; i < arr.length; ++i) {
+    total = total + arr[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+	let total = 1;
 
+  for(let i = 0; i < arr.length; ++i) {
+    total = total * arr[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+
+  for(let i = 1; i <= a; ++i) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
